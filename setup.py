987ced7901562d93ffb3ebc79b43d5a5da8952ff
pawel.trajdos@pwr.edu.pl <pawel.trajdos@pwr.edu.pl>
2024-07-16 17:29:18 +0200

    1. setup -- requirements update.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
                 'dexterous_bioprosthesis_sig_visual.*',
                 ]),
         install_requires=[ 
-                'dexterous_bioprosthesis_2021_raw_datasets @ git+https://github.com/ptrajdos/dexterous-bioprosthesis-2021-raw-dataset.git@72a8b9be4498680f024da6f57f54316b02fa89ba',
+                          'dexterous_bioprosthesis_2021_raw_datasets @ git+https://github.com/ptrajdos/dexterous-bioprosthesis-2021-raw-dataset.git@46fbb9ef296ee588c532d3960edd97ae233a69a2',
                 'matplotlib==3.8.3',
                 
         ],
